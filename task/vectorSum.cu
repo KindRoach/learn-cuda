451ff6524d685db1cf86385f8dd92ebaeb729d88
kindroach <kindroach@hotmail.com>
2021-04-28 10:07:13 +0800

    Use pointer offset.

--- task/vectorSum.cu
+++ task/vectorSum.cu
@@ -3,38 +3,45 @@
 #include "../common/utils.cuh"
 #include "../common/vectorHelper.cuh"
 
-__global__ void sumVectorOnGPU_Ver1(float *vec) {
-    unsigned offset = blockDim.x * 2 * blockIdx.x;
+__global__ void sumVectorOnGPU_Ver1(float *vec, float *res) {
+    float *vec_i = vec + blockDim.x * 2 * blockIdx.x;
     for (unsigned i = 1; i <= blockDim.x; i *= 2) {
         if (threadIdx.x % i == 0) {
-            unsigned idx = offset + threadIdx.x * 2;
-            vec[idx] = vec[idx] + vec[idx + i];
+            unsigned idx = threadIdx.x * 2;
+            vec_i[idx] = vec_i[idx] + vec_i[idx + i];
         }
         __syncthreads();
     }
+
+    if (threadIdx.x == 0) res[blockIdx.x] = vec_i[0];
 }
 
-__global__ void sumVectorOnGPU_Ver2(float *vec) {
-    unsigned offset = blockDim.x * 2 * blockIdx.x;
+__global__ void sumVectorOnGPU_Ver2(float *vec, float *res) {
+    float *vec_i = vec + blockDim.x * 2 * blockIdx.x;
     for (unsigned i = 1; i <= blockDim.x; i *= 2) {
         if (threadIdx.x < blockDim.x / i) {
-            unsigned idx = offset + threadIdx.x * (i << 1);
-            vec[idx] = vec[idx] + vec[idx + i];
+            unsigned idx = threadIdx.x * (i << 1);
+            vec_i[idx] = vec_i[idx] + vec_i[idx + i];
         }
         __syncthreads();
     }
+
+    if (threadIdx.x == 0) res[blockIdx.x] = vec_i[0];
 }
 
-__global__ void sumVectorOnGPU_Ver3(float *vec) {
-    unsigned offset = blockDim.x * 2 * blockIdx.x;
+__global__ void sumVectorOnGPU_Ver3(float *vec, float *res) {
+    float *vec_i = vec + blockDim.x * 2 * blockIdx.x;
     for (unsigned i = blockDim.x; i > 0; i /= 2) {
         if (threadIdx.x < i) {
-            unsigned idx = offset + threadIdx.x;
-            vec[idx] = vec[idx] + vec[idx + i];
+            unsigned idx = threadIdx.x;
+            vec_i[idx] = vec_i[idx] + vec_i[idx + i];
         }
         __syncthreads();
     }
+
+    if (threadIdx.x == 0) res[blockIdx.x] = vec_i[0];
 }
+
 
 void performVectorSum(size_t nElement, size_t nThread) {
     auto vec = std::vector<float>(nElement);
@@ -44,83 +51,54 @@ void performVectorSum(size_t nElement, size_t nThread) {
     std::cout << "sumVectorOnCPU:";
     TIME([&]() {
         cpuResult = std::accumulate(vec.begin(), vec.end(), 0.f);
+        printf(" %f --- ", cpuResult);
     });
 
-    // padding 0
+
+    // padding 0 for GPU
     size_t nBlock = (nElement + nThread * 2 - 1) / (nThread * 2);
     nElement = nBlock * nThread * 2;
-    auto gpuOutput = std::vector<float>(nElement);
     for (size_t i = vec.size(); i < nElement; ++i) {
         vec.emplace_back(0.f);
     }
 
-    size_t nBytes = nElement * sizeof(float);
-
-    float *d_vec;
-    cudaMalloc(&d_vec, nBytes);
+    float *d_vec, *d_result;
+    cudaMalloc(&d_vec, nElement * sizeof(float));
+    cudaMalloc(&d_result, nBlock * sizeof(float));
     CHECK(cudaGetLastError())
 
-    float gpuResult_ver1 = 0;
-    cudaMemcpy(d_vec, vec.data(), nBytes, cudaMemcpyHostToDevice);
+    auto ver1_func = [&]() { sumVectorOnGPU_Ver1<<<nBlock, nThread>>>(d_vec, d_result); };
+    auto ver2_func = [&]() { sumVectorOnGPU_Ver2<<<nBlock, nThread>>>(d_vec, d_result); };
+    auto ver3_func = [&]() { sumVectorOnGPU_Ver3<<<nBlock, nThread>>>(d_vec, d_result); };
+    auto gpuOutput = std::vector<float>(nBlock);
 
-    std::cout << "sumVectorOnGPU_Ver1:";
-    TIME([&]() {
-        sumVectorOnGPU_Ver1<<<nBlock, nThread>>>(d_vec);
-        cudaDeviceSynchronize();
-        CHECK(cudaGetLastError())
-    });
+    auto sumVector = [&](auto kernel) {
+        TIME([&]() {
+            printf("sumVectorOnGPU:");
 
-    cudaMemcpy(gpuOutput.data(), d_vec, nBytes, cudaMemcpyDeviceToHost);
-    CHECK(cudaGetLastError())
+            cudaMemcpy(d_vec, vec.data(), nElement * sizeof(float), cudaMemcpyHostToDevice);
+            kernel();
+            cudaDeviceSynchronize();
+            CHECK(cudaGetLastError())
 
-    gpuResult_ver1 = 0;
-    for (size_t i = 0; i < nElement; i += 2 * nThread) {
-        gpuResult_ver1 += gpuOutput[i];
-    }
+            cudaMemcpy(gpuOutput.data(), d_result, nBlock * sizeof(float), cudaMemcpyDeviceToHost);
+            CHECK(cudaGetLastError())
 
+            float gpuResult = 0;
+            for (size_t i = 0; i < nBlock; ++i) {
+                gpuResult += gpuOutput[i];
+            }
 
-    float gpuResult_ver2 = 0;
-    cudaMemcpy(d_vec, vec.data(), nBytes, cudaMemcpyHostToDevice);
+            printf(" %f --- ", gpuResult);
+        });
+    };
 
-    std::cout << "sumVectorOnGPU_Ver2:";
-    TIME([&]() {
-        sumVectorOnGPU_Ver2<<<nBlock, nThread>>>(d_vec);
-        cudaDeviceSynchronize();
-        CHECK(cudaGetLastError())
-    });
-
-    cudaMemcpy(gpuOutput.data(), d_vec, nBytes, cudaMemcpyDeviceToHost);
-    CHECK(cudaGetLastError())
-
-    gpuResult_ver2 = 0;
-    for (size_t i = 0; i < nElement; i += 2 * nThread) {
-        gpuResult_ver2 += gpuOutput[i];
-    }
-
-
-    float gpuResult_ver3 = 0;
-    cudaMemcpy(d_vec, vec.data(), nBytes, cudaMemcpyHostToDevice);
-
-    std::cout << "sumVectorOnGPU_Ver2:";
-    TIME([&]() {
-        sumVectorOnGPU_Ver3<<<nBlock, nThread>>>(d_vec);
-        cudaDeviceSynchronize();
-        CHECK(cudaGetLastError())
-    });
-
-    cudaMemcpy(gpuOutput.data(), d_vec, nBytes, cudaMemcpyDeviceToHost);
-    CHECK(cudaGetLastError())
-
-    gpuResult_ver3 = 0;
-    for (size_t i = 0; i < nElement; i += 2 * nThread) {
-        gpuResult_ver3 += gpuOutput[i];
-    }
-
-
-    printf("cpuResult: %f\ngpuResult_ver1: %f\ngpuResult_ver2: %f\ngpuResult_ver3: %f\n",
-           cpuResult, gpuResult_ver1, gpuResult_ver2, gpuResult_ver3);
+    sumVector(ver1_func);
+    sumVector(ver2_func);
+    sumVector(ver3_func);
 
     cudaFree(d_vec);
+    cudaFree(d_result);
 }
 
 
