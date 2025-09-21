dd37a6073bc212c7cb87cd0b064e5ba25575c62e
KindRoach <kindroach@hotmail.com>
2025-09-21 20:19:54 +0800

    Add vector_copy_naive_vec and vector_copy_multi_ele.

--- src/004-vector/vector-copy.cu
+++ src/004-vector/vector-copy.cu
@@ -4,7 +4,7 @@
 #include "util/cuda-util.cuh"
 
 template<typename T>
-__global__ void vector_copy_naive(T *src, T *dst) {
+__global__ void vector_copy_naive_kernel(T *src, T *dst) {
     size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
     dst[idx] = src[idx];
 }
@@ -17,16 +17,52 @@ void vector_copy_naive(
     size_t size = src.size();
     check_divisible(size, BLOCK_SIZE, "Global size must be divisible by BLOCK_SIZE");
     size_t blocksPerGrid = size / BLOCK_SIZE;
-    vector_copy_naive<<<blocksPerGrid, BLOCK_SIZE>>>(
+    vector_copy_naive_kernel<T><<<blocksPerGrid, BLOCK_SIZE>>>(
         thrust::raw_pointer_cast(src.data()),
         thrust::raw_pointer_cast(out.data())
     );
 }
 
+template<typename T, typename vec_t, size_t BLOCK_SIZE>
+void vector_copy_naive_vec(
+    thrust::device_vector<T> &src,
+    thrust::device_vector<T> &out
+) {
+    size_t size = src.size();
+    size_t vec_size = sizeof(vec_t) / sizeof(T);
+    check_divisible(size, BLOCK_SIZE * vec_size, "Global size must be divisible by BLOCK_SIZE * vec_size");
+    size_t blocksPerGrid = size / (BLOCK_SIZE * vec_size);
+    vector_copy_naive_kernel<vec_t><<<blocksPerGrid, BLOCK_SIZE>>>(
+        reinterpret_cast<vec_t *>(thrust::raw_pointer_cast(src.data())),
+        reinterpret_cast<vec_t *>(thrust::raw_pointer_cast(out.data()))
+    );
+}
+
+template<typename T, size_t BLOCK_SIZE, size_t THREAD_SIZE>
+__global__ void vector_copy_multi_ele_kernel(T *src, T *dst) {
+    size_t offset = blockIdx.x * blockDim.x * THREAD_SIZE + threadIdx.x;
+    for (size_t i = 0; i < BLOCK_SIZE * THREAD_SIZE; i += BLOCK_SIZE) {
+        dst[offset + i] = src[offset + i];
+    }
+}
+
+template<typename T, size_t BLOCK_SIZE, size_t THREAD_SIZE>
+void vector_copy_multi_ele(thrust::device_vector<T> &src, thrust::device_vector<T> &out) {
+    size_t size = src.size();
+    check_divisible(size, BLOCK_SIZE * THREAD_SIZE, "Global size must be divisible by BLOCK_SIZE * THREAD_SIZE");
+    size_t blocksPerGrid = size / (BLOCK_SIZE * THREAD_SIZE);
+    vector_copy_multi_ele_kernel<T, BLOCK_SIZE, THREAD_SIZE><<<blocksPerGrid, BLOCK_SIZE>>>(
+        thrust::raw_pointer_cast(src.data()),
+        thrust::raw_pointer_cast(out.data())
+    );
+}
+
+
 int main() {
     using dtype = float;
     using d_vec = thrust::device_vector<dtype>;
     constexpr uint16_t block_size = 256;
+    constexpr uint16_t thread_size = 4;
 
     size_t secs = 10;
     size_t loop = 1000;
@@ -41,6 +77,8 @@ int main() {
     using func_t = std::function<void(d_vec &, d_vec &)>;
     std::vector<std::tuple<std::string, func_t> > funcs{
         {"vector_copy_naive", vector_copy_naive<dtype, block_size>},
+        {"vector_copy_naive_vec", vector_copy_naive_vec<dtype, float4, block_size>},
+        {"vector_copy_multi_ele", vector_copy_multi_ele<dtype, block_size, thread_size>}
     };
 
     for (auto [func_name,func]: funcs) {
