8e3463de77b12c1a5f964bead7636114ee434353
KindRoach <kindroach@hotmail.com>
2025-09-22 22:22:39 +0800

    Add host-device-memory-copy.

---
+++ src/003-memory/host-device-memory-copy.cu
@@ -0,0 +1,43 @@
+#include <functional>
+#include <iostream>
+
+#include "util/bench.hpp"
+#include "util/cuda-util.cuh"
+#include "util/vector.hpp"
+
+void naive_copy(void *src, void *dst, size_t bytes) {
+    cudaMemcpy(dst, src, bytes, cudaMemcpyHostToDevice);
+}
+
+int main() {
+    using dtype = float;
+
+    size_t secs = 10;
+    size_t loop = 1000;
+    size_t size = 1000 * 1024 * 1024; // 1G elements
+    size_t bytes = size * sizeof(dtype);
+
+    std::vector<dtype> h_vec(size);
+    random_fill(h_vec);
+
+    dtype *h_pinned;
+    cuda_check(cudaMallocHost(&h_pinned, bytes));
+    std::copy(h_vec.begin(), h_vec.end(), h_pinned);
+
+    thrust::device_vector<dtype> d_vec(size);
+
+    using func_t = std::function<void()>;
+    std::vector<std::tuple<std::string, dtype *> > funcs{
+        {"normal host memory", h_vec.data()},
+        {"pinned host memory", h_pinned}
+    };
+
+    for (auto [memory_name,src_ptr]: funcs) {
+        std::cout << "\n" << memory_name << ":\n";
+        thrust::fill(d_vec.begin(), d_vec.end(), 0);
+        benchmark_func_by_time(secs, [&]() {
+            naive_copy(src_ptr, thrust::raw_pointer_cast(d_vec.data()), bytes);
+        });
+        cuda_acc_check(h_vec, d_vec);
+    }
+}
